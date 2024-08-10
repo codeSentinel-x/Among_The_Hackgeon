a7a9330a4373b87a3a118cded55ec836f40b38b9
CodeSentinel-x <dawid.fratczak@proton.me>
2024-08-10 06:52:35 +0200

    exploring

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera _cam;
+    public CinemachineConfiner2D _confirmed;
+    public RoomController _currentRoom;
+}
--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MyUtils.Enums;
@@ -5,6 +6,27 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour {
     public RoomType _roomType;
-    public BoxCollider2D _cameraBoundaries;
+    public GameObject _content;
+    public bool _found;
+    public PolygonCollider2D _cameraBoundaries;
     public DoorController[] _doors;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            SetupRoom(other.gameObject.GetComponent<PlayerController>());
+        }
+    }
+
+    private void SetupRoom(PlayerController contr) {
+        if (contr._currentRoom == this) return;
+        if (!_found) RoomFound();
+        contr._confirmed.m_BoundingShape2D = _cameraBoundaries;
+        contr._currentRoom = this;
+        Debug.Log($"Player entered {gameObject.name}");
+    }
+
+    private void RoomFound() {
+        _found = true;
+        _content.SetActive(true);
+    }
 }
