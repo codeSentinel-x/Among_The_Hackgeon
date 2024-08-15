af844896e27ca84d8b4b1db1596c423225085a9f
CodeSentinel-x <dawid.fratczak@proton.me>
2024-08-15 22:54:30 +0200

    cameraMovement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    public float _maxX;
-    public float _minX;
-    public float _maxY;
-    public float _minY;
-    public Transform _cameraTransform;
-    public float _mouseOffset;
-    public float _speed;
-    public bool _followMouse;
+public class CameraController : MonoBehaviour {
 
-    public void Update(){
-        _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, M)
+    public Transform _cameraFollow;
+    public float _mouseOffset;
+    public float _maxMouseOffset;
+    public float _speed;
+    public Vector3 _cameraOffset;
+    void Start() {
+        // _initOffset = transform.position - _cameraFollow.position;
+    }
+    public void LateUpdate() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new(Input.mousePosition.x, Input.mousePosition.y, -10));
+        Vector3 targetPos = _cameraFollow.position + _cameraOffset;
+        Vector3 mouseOffset = mousePos - targetPos;
+        mouseOffset = Vector3.ClampMagnitude(mouseOffset, _maxMouseOffset);
+        targetPos += mouseOffset * _mouseOffset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, _speed * Time.deltaTime);
     }
 }
