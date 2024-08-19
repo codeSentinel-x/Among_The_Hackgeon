9e16d8804f006591827e0b11d07f42a42280111f
CodeSentinel-x <dawid.fratczak@proton.me>
2024-08-19 20:55:54 +0200

    moreParticle

--- Assets/Scripts/GameDataManager.cs
+++ Assets/Scripts/GameDataManager.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 public class GameDataManager : MonoBehaviour {
 
     public static GameDataManager _I;
+    [Header("Door sprites")]
     public Sprite[] _destroyableDoorSpritesHorizontal;
     public Sprite[] _destroyableDoorSpritesVerticalLeft;
     public Sprite[] _destroyableDoorSpritesVerticalRight;
     public Sprite[] _closedDoorSprite;
     public Sprite[] _openedDoorSprite; //0 - up; 1- right, 2 - down; 3 - left
+    [Header("Enemy and other things")]
     public EnemyPrefabGetter _enemyPref;
     public Sprite _bulletSprite;
     public Sprite _blankSprite;
     public Transform _damageParticle;
     public Canvas _endCanvas;
+    [Header("Items")]
     public Transform[] _weaponsPrefab;
     public Transform[] _specialItemPrefab;
     public Transform[] _UtilityItemPrefab;
     public Transform _chestPrefab;
     public Transform _bossKeyPrefab;
+    [Header("Audio")]
+    public AudioClip[] _shootAudio;
+    public AudioClip[] _doorOpenAudio;
+    public AudioClip[] _music;
+    public AudioClip[] _enemySpawn;
+    public AudioClip[] _enemyDie;
+    public AudioClip[] _playerDamage;
+    public AudioClip _loopReset;
     void Awake() {
         _I = this;
     }
