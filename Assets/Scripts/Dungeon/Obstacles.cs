078a3d8554c87108c7c41c99efd0f6e0a11404ac
CodeSentinel-x <dawid.fratczak@proton.me>
2024-08-24 22:52:21 +0200

    Obstacles #1

--- Assets/Scripts/Dungeon/Obstacles.cs
+++ Assets/Scripts/Dungeon/Obstacles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MyUtils.Enums;
@@ -5,28 +6,61 @@ using MyUtils.Interfaces;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D), typeof(SpriteRenderer))]
-public class Obstacles : MonoBehaviour, IDamageable, IInteractable
-{
+public class Obstacles : MonoBehaviour, IDamageable, IInteractable {
     public ObstacleType _type;
     public int _durability;
     public int _pushPower;
     private BoxCollider2D _collider;
     private Rigidbody2D _rgb;
     private SpriteRenderer _renderer;
-    void Awake(){
-        _collider = GetComponent<BoxCollider2D>();        
+    void Awake() {
+        _collider = GetComponent<BoxCollider2D>();
         _renderer = GetComponent<SpriteRenderer>();
         _rgb = GetComponent<Rigidbody2D>();
     }
     public void Damage(float v) {
-        _durability -= 1;
-        if (_durability <= 0) Destroy();
-    }
-    public void Interact() {
-        _rgb.AddForce((transform.position - PlayerController._I.transform.position).normalized * _pushPower, ForceMode2D.Impulse);
+        switch (_type) {
+            case ObstacleType.PushableExploding: {
+                    _durability -= 1;
+                    if (_durability <= 0) Explode();
+                    break;
+                }
+            case ObstacleType.PushableDestroyable: {
+                    _durability -= 1;
+                    if (_durability <= 0) Destroy();
+                    break;
+                }
+            case ObstacleType.StaticDestroyable: {
+                    _durability -= 1;
+                    if (_durability <= 0) Destroy();
+                    break;
+                }
+        }
     }
 
-    public void Destroy(){
+    private void Explode() {
+        throw new NotImplementedException();
+    }
+
+    public void Interact() {
+        switch (_type) {
+            case ObstacleType.Pushable: {
+                    _rgb.AddForce((transform.position - PlayerController._I.transform.position).normalized * _pushPower, ForceMode2D.Impulse);
+                    break;
+                }
+            case ObstacleType.PushableExploding: {
+                    _rgb.AddForce((transform.position - PlayerController._I.transform.position).normalized * _pushPower, ForceMode2D.Impulse);
+                    break;
+
+                }
+            case ObstacleType.PushableDestroyable: {
+                    _rgb.AddForce((transform.position - PlayerController._I.transform.position).normalized * _pushPower, ForceMode2D.Impulse);
+                    break;
+                }
+        }
+    }
+
+    public void Destroy() {
         Destroy(gameObject);
     }
 }
