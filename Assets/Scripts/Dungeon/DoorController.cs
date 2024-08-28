432c7e0b26574e8edccb737ffe78b210f18a931f
CodeSentinel-x <dawid.fratczak@proton.me>
2024-08-28 21:22:10 +0200

    Door Fixes #4

--- Assets/Scripts/Dungeon/DoorController.cs
+++ Assets/Scripts/Dungeon/DoorController.cs
@@ -17,17 +17,15 @@ public class DoorController : MonoBehaviour, IDamageable, IDoor {
     public Action _uncloseDoor;
     public Action _damage;
     void Start() {
-        // _roomToShow2 = transform.parent.GetComponent<RoomController>();
-        // Initialize();
-        GetComponent<AudioSource>().volume = GameManager._gSettings._soundsVolume;
-    }
-    void Close() {
-        // GetComponent<SpriteRenderer>().
     }
     public void Initialize() {
         if (_initialized) return;
         _initialized = true;
         if (_doorType != DoorOpenType.BossRoom && _doorType != DoorOpenType.BossRoomDoors) _doorType = UnityEngine.Random.Range(0f, 1f) > 0.5f ? DoorOpenType.OpenOnShoot : DoorOpenType.AlwaysOpen;
+        SwitchRooms();
+        _init?.Invoke();
+    }
+    public void SwitchRooms() {
         switch (_doorType) {
             case DoorOpenType.AlwaysOpen: {
                     _opened = false;
@@ -39,22 +37,8 @@ public class DoorController : MonoBehaviour, IDamageable, IDoor {
                     _closeDoor = DefaultClose;
                     _uncloseDoor = DefaultUnclose;
                     _openDoor = DefaultOpen;
-                    _init = () => {
-                        _stage = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
-                            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight.Length - 1,
-                            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
-                            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft.Length - 1,
-                            _ => 0,
-                        };
-                        _renderer.sprite = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal[_stage],
-                            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight[_stage],
-                            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal[_stage],
-                            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft[_stage],
-                            _ => _gMD._openedDoorSprite[0],
-                        };
-                    };
+                    _init = DefaultInit;
+
                     break;
                 }
             case DoorOpenType.OpenOnShoot: {
@@ -63,20 +47,7 @@ public class DoorController : MonoBehaviour, IDamageable, IDoor {
                         _roomToShow._onPlayerEnter += () => {
                             if (!_roomToShow._wasInvoked) _closeDoor?.Invoke();
                         };
-                    _closeDoor = () => {
-                        _hidden = true;
-                        if (_opened) {
-                            _col.isTrigger = false;
-                            // _renderer.enabled = true;
-                            _renderer.sprite = _pos switch {
-                                _ when _pos == DoorPosition.Up => _gMD._closedDoorSprite[0],
-                                _ when _pos == DoorPosition.Right => _gMD._closedDoorSprite[1],
-                                _ when _pos == DoorPosition.Down => _gMD._closedDoorSprite[2],
-                                _ when _pos == DoorPosition.Left => _gMD._closedDoorSprite[3],
-                                _ => _gMD._closedDoorSprite[0],
-                            };
-                        }
-                    };
+                    _closeDoor = DefaultClose;
                     _uncloseDoor = () => {
                         _hidden = false;
                         if (!_discovered) return;
@@ -90,54 +61,18 @@ public class DoorController : MonoBehaviour, IDamageable, IDoor {
                             _ => _gMD._openedDoorSprite[0],
                         };
                     };
-                    _openDoor = () => {
-                        _opened = true;
-                        _discovered = true; ;
-                        _col.isTrigger = true;
-                        _renderer.sprite = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._openedDoorSprite[0],
-                            _ when _pos == DoorPosition.Right => _gMD._openedDoorSprite[1],
-                            _ when _pos == DoorPosition.Down => _gMD._openedDoorSprite[2],
-                            _ when _pos == DoorPosition.Left => _gMD._openedDoorSprite[3],
-                            _ => _gMD._openedDoorSprite[0],
-                        };
-                        var d = GetComponent<DoorController>();
-                        if (d._tunnelToShow._maskTransform != null) d._tunnelToShow._maskTransform.gameObject.SetActive(false);
-                        d._tunnelToShow.ShowRoom();
-                        d._roomToShow.ShowRoom();
-                    };
-                    _init = () => {
-                        _stage = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
-                            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight.Length - 1,
-                            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
-                            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft.Length - 1,
-                            _ => 0,
-                        };
-                        _renderer.sprite = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal[_stage],
-                            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight[_stage],
-                            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal[_stage],
-                            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft[_stage],
-                            _ => _gMD._openedDoorSprite[0],
-                        };
-                    };
+                    _openDoor = DefaultOpen;
+                    _init = DefaultInit;
+
                     break;
                 }
             case DoorOpenType.BossRoom: {
                     _opened = false;
-                    _closeDoor = () => {
-                        //TODO
-                    };
-                    _uncloseDoor = () => {
-                        //todo
-                    };
-                    _openDoor = () => {
-                        //todo
-                    };
-                    _init = () => {
+                    _closeDoor = DefaultClose;
+                    _uncloseDoor = DefaultUnclose;
+                    _openDoor = DefaultOpen;
+                    _init = DefaultInit;
 
-                    };
                     break;
                 }
             case DoorOpenType.BossRoomDoors: {
@@ -145,31 +80,11 @@ public class DoorController : MonoBehaviour, IDamageable, IDoor {
                     _roomToShow._onPlayerEnter += () => {
                         if (!_roomToShow._wasInvoked) _closeDoor?.Invoke();
                     };
-                    _closeDoor = () => {
-                        //TODO
-                    };
-                    _uncloseDoor = () => {
-                        //todo
-                    };
-                    _openDoor = () => {
-                        //todo
-                    };
-                    _init = () => {
-                        _stage = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
-                            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight.Length - 1,
-                            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
-                            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft.Length - 1,
-                            _ => 0,
-                        };
-                        _renderer.sprite = _pos switch {
-                            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal[_stage],
-                            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight[_stage],
-                            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal[_stage],
-                            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft[_stage],
-                            _ => _gMD._openedDoorSprite[0],
-                        };
-                    };
+                    _closeDoor = DefaultClose;
+                    _uncloseDoor = DefaultUnclose;
+                    _openDoor = DefaultOpen;
+                    _init = DefaultInit;
+
 
                     break;
                 }
@@ -339,6 +254,9 @@ public class DoorController : MonoBehaviour, IDamageable, IDoor {
         if (d._tunnelToShow._maskTransform != null) d._tunnelToShow._maskTransform.gameObject.SetActive(false);
         d._tunnelToShow.ShowRoom();
         d._roomToShow.ShowRoom();
+    }
+    public void DefaultDamage() {
+
     }
 }
 public enum DoorPosition {
