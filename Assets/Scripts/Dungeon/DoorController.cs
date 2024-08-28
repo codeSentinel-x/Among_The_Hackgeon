d211e93ff5b370f2955cd32db60bc9b609e75d0c
CodeSentinel-x <dawid.fratczak@proton.me>
2024-08-28 07:36:44 +0200

    Fixes

--- Assets/Scripts/Dungeon/DoorController.cs
+++ Assets/Scripts/Dungeon/DoorController.cs
@@ -96,6 +96,98 @@ public class DoorController : MonoBehaviour {
                 }
         }
     }
+
+    public int _stage;
+    private AssetManager _gMD;
+    private SpriteRenderer _renderer;
+    public bool _opened;
+    private bool _discovered = false;
+    public DoorPosition _pos;
+    private bool _hidden;
+    void Start() {
+        _gMD = AssetManager._I;
+        _stage = _pos switch {
+            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
+            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight.Length - 1,
+            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal.Length - 1,
+            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft.Length - 1,
+            _ => 0,
+        };
+        _col = GetComponent<Collider2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.sprite = _pos switch {
+            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal[_stage],
+            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight[_stage],
+            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal[_stage],
+            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft[_stage],
+            _ => _gMD._openedDoorSprite[0],
+        };
+        _col.isTrigger = false;
+        _opened = false;
+        _discovered = false;
+        GetComponent<DoorController>()._roomToShow._onRoomClear += (x) => { if (x == GetComponent<DoorController>()._roomToShow) UncloseDoor(); };
+    }
+    public void CloseDoor() {
+        _hidden = true;
+        if (_opened) {
+            _col.isTrigger = false;
+            // _renderer.enabled = true;
+            _renderer.sprite = _pos switch {
+                _ when _pos == DoorPosition.Up => _gMD._closedDoorSprite[0],
+                _ when _pos == DoorPosition.Right => _gMD._closedDoorSprite[1],
+                _ when _pos == DoorPosition.Down => _gMD._closedDoorSprite[2],
+                _ when _pos == DoorPosition.Left => _gMD._closedDoorSprite[3],
+                _ => _gMD._closedDoorSprite[0],
+            };
+        }
+    }
+
+    public void Damage(float v) {
+        if (_opened || _hidden) return;
+        if (_stage == 0) { OpenDoor(); AudioManager._I.PlaySoundEffect(AudioType.DoorOpen, transform.position); return; }
+        _stage--;
+        _renderer.sprite = _pos switch {
+            _ when _pos == DoorPosition.Up => _gMD._destroyableDoorSpritesHorizontal[_stage],
+            _ when _pos == DoorPosition.Right => _gMD._destroyableDoorSpritesVerticalRight[_stage],
+            _ when _pos == DoorPosition.Down => _gMD._destroyableDoorSpritesHorizontal[_stage],
+            _ when _pos == DoorPosition.Left => _gMD._destroyableDoorSpritesVerticalLeft[_stage],
+            _ => _gMD._openedDoorSprite[0],
+        };
+
+
+
+    }
+
+    public void OpenDoor() {
+        _opened = true;
+        _discovered = true; ;
+        _col.isTrigger = true;
+        _renderer.sprite = _pos switch {
+            _ when _pos == DoorPosition.Up => _gMD._openedDoorSprite[0],
+            _ when _pos == DoorPosition.Right => _gMD._openedDoorSprite[1],
+            _ when _pos == DoorPosition.Down => _gMD._openedDoorSprite[2],
+            _ when _pos == DoorPosition.Left => _gMD._openedDoorSprite[3],
+            _ => _gMD._openedDoorSprite[0],
+        };
+        var d = GetComponent<DoorController>();
+        if (d._tunnelToShow._maskTransform != null) d._tunnelToShow._maskTransform.gameObject.SetActive(false);
+        d._tunnelToShow.ShowRoom();
+        d._roomToShow.ShowRoom();
+    }
+
+    public void UncloseDoor() {
+        _hidden = false;
+        if (!_discovered) return;
+        _opened = true;
+        _col.isTrigger = true;
+        _renderer.sprite = _pos switch {
+            _ when _pos == DoorPosition.Up => _gMD._openedDoorSprite[0],
+            _ when _pos == DoorPosition.Right => _gMD._openedDoorSprite[1],
+            _ when _pos == DoorPosition.Down => _gMD._openedDoorSprite[2],
+            _ when _pos == DoorPosition.Left => _gMD._openedDoorSprite[3],
+            _ => _gMD._openedDoorSprite[0],
+        };
+    }
 }
 public class DoorBasic : MonoBehaviour {
     public DoorState state;
